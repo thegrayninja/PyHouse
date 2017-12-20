4076b56a3ef3d1ecfe5da9ca2b19ca6e309f99d3
thegrayninja <grayninja@protonmail.com>
2017-12-20 15:49:36 -0800

    Update csv_to_html.py

--- csv_to_html.py
+++ csv_to_html.py
@@ -1,6 +1,8 @@
 DocContents = []
 import csv
-with open('.\Docs\workbook3.csv', 'rt', encoding='utf8') as csvfile:
+#if encoding is required, replace with this line:
+#with open('.\\files\workbook2.csv', 'rt', encoding='utf8') as csvfile:
+with open('.\\files\workbook2.csv', 'rt') as csvfile:
     CSVData = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in CSVData:
         DocContents.append(row)
@@ -9,27 +11,48 @@ with open('.\Docs\workbook3.csv', 'rt', encoding='utf8') as csvfile:
 
 
 Count = 0
+ToC = ""
+Content = ""
 for row in DocContents:
     Word = ""
     for i in DocContents[Count][0]:
         Word += i
         Words = Word.split()
+
+
     for a in "a":
         DotCount = 0
         for i in Words[0]:
             if i == ".":
                 DotCount += 1
         if DotCount == 1:
-            Prefix = '<div id="level1tab"><a href=test.html#%s>' % (Words[0])
-            Suffix = '<a href=test.html#%s></div>' % (Words[0])
+            Prefix = '<div id="level1tab"><a href=audit_helper.html#%s>' % (Words[0])
+            Suffix = '</a></div>'
         elif DotCount == 2:
-            Prefix = '<div id="level2tab"><a href=test.html#%s>' % (Words[0])
-            Suffix = '<a href=test.html#%s></div>' % (Words[0])
+            Prefix = '<div id="level2tab"><a href=audit_helper.html#%s>' % (Words[0])
+            Suffix = '</a></div>'
+        elif DotCount == 3:
+            Prefix = '<div id="level3tab"><a href=audit_helper.html#%s>' % (Words[0])
+            Suffix = '</a></div>'
 
         else:
-            Prefix = ""
+            Prefix = "<br><br>"
             Suffix = "<br>"
+        try:
+            Content += "<p id=%s>" % (Words[0]) + DocContents[Count][0] + "</p></ br> </ br>\n"
+        except:
+            Content += "\n"
 
-    print(Prefix + Words[0] + " - " + " ".join(Words[1:5]) + "..." + Suffix)
-
+    ToC += Prefix + Words[0] + " - " + " ".join(Words[1:5]) + "..." + Suffix + "\n"
     Count += 1
+WebContent = ""
+WebContent += '<html><head><title>Prioritized Approach to Stuff</title><link rel="stylesheet" href="basic.css" type="text/css" /> </head>'
+WebContent += "<body>"
+WebContent += ToC
+WebContent += "\n\n"
+WebContent += Content
+WebContent += "</body></html>"
+
+PageHTML = open(".\\files\\audit_helper.html", "w")
+PageHTML.write(WebContent)
+PageHTML.close()
