a539dad9878242496f856653e1d701b185dbd2d6
thegrayninja <grayninja@protonmail.com>
2018-01-27 17:17:16 -0800

    Update nmap_automation.py

--- nmap_automation.py
+++ nmap_automation.py
@@ -1,129 +1,203 @@
-#nmap scanning automation tool
+# automate scan tools
 
-#goal is to segment types of scans based on open ports. This intelligence
-#has not been coded as of yet....
-
-
-import os
-import subprocess
-import sys
-
-import time
-
-
-IPAddress = sys.argv[1]
-NmapResults = ""
-NmapWebScanStatus = False
-NmapSSLScanStatus = False
-
-WebOnlyScanPortsList = ["80", "8080", "8081", "8181"]
-WebScanPortsList = ["80", "8080", "8081", "8181", "443", "4443"]
-SSLScanPortsList = ["443", "4443"]
-
-
-WebPorts = "-p 80,8080,8081,8181,443,4443 "
-WebOnlyPorts = "-p 80,8080,8081,8181 "
-SSLPorts = "-p 443,4443 "
-
-ResultsFullBreak = "\n\n\n" + "*" * 20 + "\n" + "*" * 20 + "\n\n"
-ResultsHeaderBreak = "\n\n" + "*" * 20 + "\n"
-
-OpenPorts = []
-
-NmapResults += "Scans initiated at " + time.strftime('%X %x %Z') + "\n\n"
-
-NmapScans = ["nmap -A -O -PN ", "nmap -Pn -sP --script discovery ", "nmap -p 443 --script ssl-heartbleed ", "nmap -Pn --script http-shellshock "]
-
-NmapWebScans = ["nmap -Pn --script http-apache-negotiation ", "nmap -Pn --script http-apache-server-status ", "nmap -Pn --script http-cookie-flags ",
-                "nmap -Pn --script http-cors ", "nmap -Pn --script http-cross-domain-policy ", "nmap -Pn --script http-enum ",
-                "nmap -Pn --script http-headers ", "nmap -Pn --script http-php-version ", "nmap -Pn --script http-robots.txt ",
-                "nmap -Pn --script http-security-headers ", "nmap -Pn --script snmp-info "]
-
-NmapSSLScans = ["nmap -Pn --script ssl-enum-ciphers ", "nmap -Pn --script ssl-cert ", "nmap -Pn --script sslv2 "]
+import re
+import time, sys, os
+import subprocess, platform
 
 
 
-for i in WebOnlyScanPortsList:
-    GrepCut = " | grep -w " + i + "/tcp"# | cut -d' ' -f3"
-    OpenPortCheck = (subprocess.check_output(("nmap -Pn " + WebOnlyPorts + IPAddress + GrepCut), shell=True)).strip()
-    if " open " in OpenPortCheck:
-        print("Port " + i + " is open!")
-        OpenPorts.append(i)
-        NmapWebScanStatus = True
+# TODO Import file containing IP Addresses
+# TODO Run set of commands per IP Address
+# TODO Create Folder based on IP Address
+# TODO Create subfolder based on Tool used
+# TODO Determine Open Ports
+
+
+def main():
+    #CheckPythonVersion()
+    FileNameIPAddress = "ipaddress.txt"
+    IPAddresses = GetIPAddressesFromFile(FileNameIPAddress)
+    for IP in IPAddresses:
+        CommandList(IP.strip())
+
+
+
+####
+####
+#Get Host Information
+
+#TODO Current code to run shell command from python fails in v3.x
+def CheckPythonVersion():
+    PyVerTuple = sys.version_info[:1]
+    if PyVerTuple[0] > 2:
+        return 0
     else:
-        print("Port " + i + " is closed/filtered/unknown.")
+        print("\n\nPlease run Python 3.x or newer\n")
+        sys.exit(1)
+
+def GetOSVersion():
+    return (platform.system())
+
+#####
+#####
 
 
-for i in SSLScanPortsList:
-    GrepCut = " | grep -w " + i + "/tcp"# | cut -d' ' -f3"
-    OpenPortCheck = (subprocess.check_output(("nmap -Pn " + SSLPorts + IPAddress), shell=True)).strip()
-    if "open" in OpenPortCheck:
-        print("Port " + i + " is open!")
-        OpenPorts.append(i)
-        NmapSSLScanStatus = True
+####
+####
+#Admin Work
+
+def SaveDataToFile(Data, FileName):
+    SaveFile = open(FileName, "w")
+    SaveFile.write(str(Data))
+    SaveFile.close()
+    print("file has been saved")
+
+
+def CreateIPFolder(IPAddress):
+    print("...Creating a working directory...")
+    OSVersion = GetOSVersion()
+    if "Windows" in OSVersion:
+        ##TODO For Windows --doesn't work
+        SaveFolder = r'.\%s' % IPAddress
+        if not os.path.exists(SaveFolder):
+            os.makedirs(SaveFolder)
+            print("Folder .\%s has been created!" % IPAddress)
+
     else:
-        print("Port " + i + " is closed/filtered/unknown.")
+        ##TODO For Linux
+        SaveFolder = './%s' % IPAddress
+        if not os.path.exists(SaveFolder):
+            os.makedirs(SaveFolder)
+            print("Folder ./%s has been created!" % IPAddress)
 
-print("\n\n")
+def CreateToolFolder():
+    print("based on commands")
 
-
-for i in NmapScans:
-    try:
-        print("Running scan: " + i + IPAddress)
-        NmapResults += ResultsFullBreak
-        NmapResults += i + IPAddress
-        NmapResults += ResultsHeaderBreak
-        NmapResults += subprocess.check_output((i + IPAddress), shell=True)
-
-    except:
-        NmapResults += ResultsFullBreak
-        NmapResults += "Failed scan: " + i + IPAddress
-        NmapResults += ResultsHeaderBreak
-        print("Failed scan: " + i + IPAddress)
-        print("Try running as root.")
+####
+####
 
 
 
-if NmapWebScanStatus == True:
-    for i in NmapWebScans:
-        PortAndIP = i + WebPorts + IPAddress
+####
+####
+#Initial Recon
+
+def GetIPAddressesFromFile(FileNameIPAddress):
+    print("...Getting IP Address from file...")
+    IPAddressList = []
+    TempFile = open(FileNameIPAddress, 'r')
+    IPAddressList.append(TempFile.read())
+    TempFile.close()
+    return (IPAddressList)
+
+def DetermineOpenPorts(IPAddress):
+    print("...Running initial nmap scan against %s..." % IPAddress)
+    NmapCommand = "nmap -Pn -p 80,443,22 %s" % IPAddress
+    NmapResults = subprocess.check_output((NmapCommand), shell=True)
+    FileName = "./%s/OpenPorts_%s.txt" % (IPAddress, IPAddress)
+    SaveDataToFile(NmapResults.strip(), FileName)
+    print("...Initial nmap scan is complete.")
+    return FileName
+
+
+
+def GetOpenPorts(FileName):
+    print("...Trying to determine open ports...")
+    FirstTerm = "open"
+    SecondTerm = "^\d*"
+    OpenPorts = []
+
+    for line in open(FileName, 'r'):
+        if re.search(FirstTerm, line):
+            x = (re.search(SecondTerm, line).group(0))
+            if x != "":
+                OpenPorts.append(x)
+    print("Open Ports: "),
+    print(OpenPorts)
+    return OpenPorts
+####
+####
+
+
+####
+####
+#Tools to run
+
+def CommandList(IPAddress):
+    print("...Scanning against %s has started. Please be patient." % IPAddress)
+    CreateIPFolder(IPAddress)
+    FileName = DetermineOpenPorts(IPAddress)
+    OpenPorts = GetOpenPorts(FileName)
+    NmapScans(IPAddress, OpenPorts)
+
+    print("...Scanning for %s has completed." % IPAddress)
+
+def NmapScans(IPAddress, OpenPorts):
+    NmapFolderName = IPAddress + "/nmap"
+    CreateIPFolder(NmapFolderName)
+    WebPorts = ['80', '8080', '8888', '8181', '443', '8443']
+    SSLPorts = ['443', '8443']
+    MailPorts = ['25', '110']
+    RDP = ['3389']
+    NmapResults = "Nmap Scans initiated at " + time.strftime('%X %x %Z') + "\n\n"
+
+    NmapPortScans = ["nmap -A -O -Pn "]
+    NmapDiscoveryScans = ["nmap -Pn -sP --script discovery "]
+
+    NmapWebScans = ["nmap -Pn --script http-apache-negotiation ", "nmap -Pn --script http-apache-server-status ",
+                    "nmap -Pn --script http-cookie-flags ",
+                    "nmap -Pn --script http-cors ", "nmap -Pn --script http-cross-domain-policy ",
+                    "nmap -Pn --script http-enum ",
+                    "nmap -Pn --script http-headers ", "nmap -Pn --script http-php-version ",
+                    "nmap -Pn --script http-robots.txt ",
+                    "nmap -Pn --script http-security-headers ", "nmap -Pn --script snmp-info "]
+
+    NmapSSLScans = ["nmap -Pn --script ssl-enum-ciphers ", "nmap -Pn --script ssl-cert ", "nmap -Pn --script sslv2 ",
+                    "nmap --script ssl-heartbleed ", "nmap -Pn --script http-shellshock "]
+
+
+    for Scan in NmapPortScans:
+        for Port in OpenPorts:
+            ScanCommand = Scan + "-p " + Port + " " + IPAddress
+            print("Performing %s. Please be patient..." % ScanCommand)
+            try:
+                NmapResults += ScanCommand + " is initiating at " + time.strftime('%X %x %Z')
+                NmapResults += (subprocess.check_output((ScanCommand), shell=True)).strip()
+                NmapResults += "\n####################\n"
+            except:
+                print("%s FAILED!! Reasons unknown." % ScanCommand)
+
+    for Scan in NmapDiscoveryScans:
+        ScanCommand = Scan + IPAddress
+        print("Performing %s. Please be patient..." % ScanCommand)
         try:
-            print("Running scan: " + PortAndIP)
-            NmapResults += ResultsFullBreak
-            NmapResults += PortAndIP
-            NmapResults += ResultsHeaderBreak
-            NmapResults += subprocess.check_output((PortAndIP), shell=True)
-
+            NmapResults += ScanCommand + " is initiating at " + time.strftime('%X %x %Z') + "\n"
+            NmapResults += (subprocess.check_output((ScanCommand), shell=True)).strip()
+            NmapResults += "\n\n####################\n\n####################\n\n"
         except:
-            NmapResults += ResultsFullBreak
-            NmapResults += "Failed scan: " + PortAndIP
-            NmapResults += ResultsHeaderBreak
-            print("Failed scan: " + PortAndIP)
-            print("Try running as root.")
+            print("%s FAILED!! Reasons unknown." % ScanCommand)
 
-if NmapSSLScanStatus == True:
-    for i in NmapSSLScans:
-        SSLPortAndIP = i + SSLPorts + IPAddress
-        try:
-            print("Running scan: " + SSLPortAndIP)
-            NmapResults += ResultsFullBreak
-            NmapResults += SSLPortAndIP
-            NmapResults += ResultsHeaderBreak
-            NmapResults += subprocess.check_output((SSLPortAndIP), shell=True)
+    for Port in SSLPorts:
+        for OPort in OpenPorts:
+            if Port == OPort:
+                for Scan in NmapSSLScans:
+                    ScanCommand = Scan + "-p " + Port + " " + IPAddress
+                    print("Performing %s. Please be patient..." % ScanCommand)
+                    try:
+                        NmapResults += ScanCommand + " is initiating at " + time.strftime('%X %x %Z') + "\n"
+                        NmapResults += (subprocess.check_output((ScanCommand), shell=True)).strip()
+                        NmapResults += "\n\n####################\n\n####################\n\n"
+                    except:
+                        print("%s FAILED!! Reasons unknown." % ScanCommand)
 
-        except:
-            NmapResults += ResultsFullBreak
-            NmapResults += "Failed scan: " + SSLPortAndIP
-            NmapResults += ResultsHeaderBreak
-            print("Failed scan: " + SSLPortAndIP)
-            print("Try running as root.")
+    print(NmapResults)
+    NmapFileName = NmapFolderName + "/nmap_results_%s.txt" % IPAddress
+    SaveDataToFile(NmapResults, NmapFileName)
+
+####
+####
 
 
-print(NmapResults)
 
-FileName = "NmapScan_" + IPAddress + ".txt"
-SaveFile = open(FileName, "w")
-SaveFile.write(NmapResults)
-SaveFile.close()
-
-print(OpenPorts)
+if __name__ == main():
+    main()
